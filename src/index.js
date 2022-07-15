081bfadb129b6e7e18427066566c2cbbecff329c
Trizferoli <biaferoi@gmail.com>
2022-07-15 10:26:10 -0300

    att heroku port

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
 app.use(cors());
 
 const routes = require('./routes');
 
 app.use(express.json());
 app.use(routes);
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 
 
