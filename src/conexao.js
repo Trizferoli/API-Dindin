47ee906575af93f19a19a63f4a2d463a134924af
Trizferoli <biaferoi@gmail.com>
2022-07-15 10:37:50 -0300

    added heroku database

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'dindin',
-    password: '071011',
-    port: 5432
+    user: 'swcyyzzbcyseql',
+    host: 'ec2-3-224-164-189.compute-1.amazonaws.com',
+    database: 'd50hqil11uuc2g',
+    password: 'bc41b5c51c218d091f215e3a25e7ceb7c10216db91b89e65e239d133b4e66566',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const query = (text, param) => {
